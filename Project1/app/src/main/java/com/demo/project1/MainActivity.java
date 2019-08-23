c1047a4f45e8a7675ee83764d424352e20af035a
Bob <tom8982@gmail.com>
2019-08-23 20:21:24 +0800

    Create MainActivity.java

---
+++ Project1/app/src/main/java/com/demo/project1/MainActivity.java
@@ -0,0 +1,30 @@
+package com.demo.project1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Button nextPageBtn = (Button) findViewById(R.id.button2);
+
+        nextPageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, Main2Activity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
