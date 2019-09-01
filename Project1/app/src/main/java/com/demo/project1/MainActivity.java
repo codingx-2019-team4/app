ff92f4c598d72685bc49b2e2bc486e80bf3fc05f
Bob <tom8982@gmail.com>
2019-09-01 13:03:48 +0800

    Update MainActivity.java

--- Project1/app/src/main/java/com/demo/project1/MainActivity.java
+++ Project1/app/src/main/java/com/demo/project1/MainActivity.java
@@ -1,6 +1,7 @@
 package com.demo.project1;
 
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -21,10 +22,26 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent();
-                intent.setClass(MainActivity.this, Main2Activity.class);
+                intent.setClass(MainActivity.this, Main3Activity.class);
                 startActivity(intent);
             }
         });
+
+        Main3Activity.Broadcast br;
+        br = new Main3Activity.Broadcast();
+
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Main3Activity.REGISTER);
+        this.registerReceiver(br, filter);
+
+
+        Intent intent = new Intent(this, MyService.class);
+        startService(intent);
     }
 
+    public void text(View view) {
+        Intent intent = new Intent();
+        intent.setClass(MainActivity.this,Main3Activity.class);
+        startActivity(intent);
+    }
 }
