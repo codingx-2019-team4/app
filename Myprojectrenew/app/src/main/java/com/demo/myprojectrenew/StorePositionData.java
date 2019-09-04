37d2dee8cb891bf866a138fb7c7852121c1e584a
Bob <tom8982@gmail.com>
2019-09-04 12:22:31 +0800

    Create StorePositionData.java

---
+++ Myprojectrenew/app/src/main/java/com/demo/myprojectrenew/StorePositionData.java
@@ -0,0 +1,35 @@
+package com.demo.myprojectrenew;
+
+import java.util.ArrayList;
+
+public class StorePositionData {
+    public ArrayList<Position> position = new ArrayList();
+
+    private StorePositionData() {
+    }
+
+    public static StorePositionData getInstance() {
+        return StorePositionDataHolder.sInstance;
+    }
+
+    private static class StorePositionDataHolder {
+        private static final StorePositionData sInstance = new StorePositionData();
+    }
+
+    public static class Position {
+        private int x = 0, y = 0;
+
+        public Position(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int getX() {
+            return x;
+        }
+
+        public int getY() {
+            return y;
+        }
+    }
+}
